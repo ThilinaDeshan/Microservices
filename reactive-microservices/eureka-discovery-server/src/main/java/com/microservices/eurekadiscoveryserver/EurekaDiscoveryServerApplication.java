4dc5981f595c08c9336b6ff8fbd319ca2bfed8a2
ThilinaDeshan <iam.thilina.deshan@gmail.com>
2021-05-16 10:10:59 +0530

    adding eureka discovery server and its dependencies

---
+++ reactive-microservices/eureka-discovery-server/src/main/java/com/microservices/eurekadiscoveryserver/EurekaDiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.microservices.eurekadiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication0
+@EnableEurekaServer
+public class EurekaDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryServerApplication.class, args);
+	}
+
+}
